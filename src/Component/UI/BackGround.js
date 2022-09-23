3ce6e63ef0240ca812afc49c3bd3cba168b83332
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-09-23 16:48:47 +0900

    sideBar css 완성

--- src/Component/UI/BackGround.js
+++ src/Component/UI/BackGround.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hamburger from "./TopSideBtn.js/Hamburger";
 import Alert from "./TopSideBtn.js/Alert";
+import Title  from "./TopSideBtn.js/Title";
 const BackGround = () => {
   return (
     <>
     <Hamburger/>
     <Alert/>
+    <Title/>
     </>
   );
 }
---
+++ src/Component/UI/TopSideBtn.js/Title.js
@@ -0,0 +1,13 @@
+const Title = () => {
+  const spanStyle = {
+    position : "absolute",
+    margin : "1rem 6.5rem",
+
+  }
+  
+  return (
+    <span style = {spanStyle}>Maple Hands+</span>
+  )
+} 
+
+export default Title;
